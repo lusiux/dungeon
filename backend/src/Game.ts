cb29e19346d81be65d72fcd7ffdd72de154f46dd
lusiux <tm.github@atlinux.de>
2021-11-25 20:18:36 +0100

    Persist games to hard drive

--- backend/src/Game.ts
+++ backend/src/Game.ts
@@ -1,10 +1,11 @@
-import { Room } from './types'
+import { Room, Inventory as ApiInventory, getChest, getWorkbench, getSocket } from './types'
 import { Inventory } from './Inventory'
 import { v4 as uuid } from 'uuid'
 import clone from 'clone'
-
+import { writeFile, readFile } from 'fs/promises'
+import { plainToInstance } from 'class-transformer'
 export class Game {
-  private readonly inventory: Inventory
+  private inventory: Inventory
   private readonly id: string
   private readonly rooms: Record<string, Room>
 
@@ -14,8 +15,27 @@ export class Game {
     this.inventory = new Inventory()
   }
 
-  public updateCurrentRoom (roomId: string): void {
+  private setInventory (inventory: Inventory): void {
+    this.inventory = inventory
+  }
+
+  public static async fromFile (gameId: string): Promise<Game> {
+    const fileContent = await readFile(`games/game-${gameId}.json`)
+    const gameJson = JSON.parse(fileContent.toString())
+
+    const game = plainToInstance(Game, gameJson)
+    game.setInventory(plainToInstance(Inventory, gameJson.inventory))
+
+    return game
+  }
+
+  public async persist (): Promise<void> {
+    await writeFile(`games/game-${this.id}.json`, JSON.stringify(this))
+  }
+
+  public async updateCurrentRoom (roomId: string): Promise<void> {
     this.currentRoomId = roomId
+    await this.persist()
   }
 
   public getCurrentRoomId (): string {
@@ -36,5 +56,55 @@ export class Game {
 
   public getInventory (): Inventory {
     return this.inventory
+  }
+
+  public async moveToRoom (roomId: string): Promise<Room> {
+    const room: Room = clone(this.getRoom(roomId))
+    await this.updateCurrentRoom(roomId)
+
+    if (room.socket !== undefined) {
+      room.socket.targetRoom = ''
+    }
+
+    return room
+  }
+
+  public async getItems (): Promise<ApiInventory> {
+    return { inventory: this.inventory.getItems() }
+  }
+
+  public async pickChest (roomId: string): Promise<ApiInventory> {
+    const room = this.getRoom(roomId)
+    const chest = getChest(room)
+
+    if (chest.item.quantity > 0) {
+      this.inventory.addItem({ ...chest.item, quantity: 1 })
+      chest.item.quantity -= 1
+    }
+
+    await this.persist()
+
+    return { inventory: this.inventory.getItems() }
+  }
+
+  public async craft (roomId: string): Promise<ApiInventory> {
+    const room = this.getRoom(roomId)
+    const workbench = getWorkbench(room)
+
+    this.inventory.removeItemsOrThrow(workbench.inputs)
+    this.inventory.addItem(workbench.output)
+
+    await this.persist()
+    return { inventory: this.inventory.getItems() }
+  }
+
+  public async plug (roomId: string): Promise<{id: string}> {
+    const room = this.getRoom(roomId)
+    const socket = getSocket(room)
+
+    this.inventory.removeItemOrThrow(socket.item)
+
+    await this.persist()
+    return { id: socket.targetRoom }
   }
 }
